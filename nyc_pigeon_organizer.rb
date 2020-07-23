eb508ae054a44ca05e9b9efeb9250fb8332b98fc
JoAnna Park <joannabpark1@gmail.com>
2020-07-23 19:49:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,9 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+    end
+    final_array
+  end
 end
