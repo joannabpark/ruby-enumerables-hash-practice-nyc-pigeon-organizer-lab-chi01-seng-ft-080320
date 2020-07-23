4236fa89b030d1f3d2cd3b3216573ad5709bfe43
JoAnna Park <joannabpark1@gmail.com>
2020-07-23 19:54:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name] 
+          final_array[name] = {}
       end
     end
     final_array
